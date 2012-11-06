af0a4d12756f8097cea9980f3c3455f43cddbf2b
Chris P <sahib@online.de>
2012-11-06 05:29:44 +0100

    Fixed Parser, Added ws2012.list

--- practice/schedule/parser.py
+++ practice/schedule/parser.py
@@ -1,96 +1,157 @@
 #!/usr/bin/env python
-import re, urllib.request, sys
-from html.parser  import HTMLParser
-from urllib.error import HTTPError,URLError
+# encoding: utf-8
 
-"""
-Very silly parser for 
-http://www.hof-university.de/index.php?id=515&st=Inf&fs=4&jahr=2012&semester=SS
+import re, sys, urllib
+from urllib.request import urlopen
+from html.parser import HTMLParser
+from urllib.parse import quote
 
-(Hey, it works!)
-"""
+from pprint import pprint
+
+'''
+
+# Inf3
+
+11:30
+13:00
+Formale Sprachen -- -
+ - (Inf3 + WI3)
+Prof. Dr. Richard Göbel
+HF:1
+FA009
+
+
+# Inf5
+
+11:30
+13:00
+Geographische Informationssysteme -- -
+ - (Vinf+Inf+MI 5)
+Prof. Dr. Richard Göbel,  Carsten Kropf
+FWM:1
+FB001/002
+
+
+'''
+
+headers = ['room', 'type', 'time', 'note', 'prof', 'name', 'desc']
+
+
+def looks_like_room(data):
+    return re.match('F[A-Z][0-9]+', data) or re.match('Ex_*', data)
+
+
+def guess_key_from_data(data):
+    data = data.strip()
+    if looks_like_room(data):
+        return 'room'
+    elif re.match('[A-Z]+:[0-9]', data):
+        return 'type'
+    elif re.match('[0-9][0-9]:[0-9][0-9]', data):
+        return 'time'
+    elif re.search('\(.+\)*.', data) and len(data) < 20:
+        return 'note'
+    else:
+        # Not yet defined. Done while sanitizing.
+        return '_tmp'
+
+
 class TableHTMLParser(HTMLParser):
     def __init__(self):
-        super(TableHTMLParser,self).__init__()
-        self.interest = False
-        self.lastday  = "Montag"
-        self.dataset  = []
-        self.plandict = {}
-
-    def __add_record(self,data):
-        self.plandict[self.lastday] = self.dataset
-        self.dataset = []
-        self.lastday = data
+        HTMLParser.__init__(self)
+        self.__interest = False
+        self.schedule = {}
+        self._current_list = []
+        self._current_idx = 0
+        self._node_idx = 0
 
     def handle_starttag(self, tag, attrs):
-        if tag == 'td' and ('class','data') in attrs:
-            self.interest = True
+        # Be only interested in this <td> tags,
+        # that have class= and data= attributes.
+        if tag == 'td' and ('class', 'data') in attrs:
+            self.__interest = True
 
     def handle_endtag(self, tag):
-        self.interest = False
+        # Always be uninterested by default.
+        self.__interest = False
 
     def handle_data(self, data):
-        if self.interest and data != '\n':
-            data = data.strip()
-            if data in ["Montag","Dienstag","Mittwoch","Donnerstag","Freitag"]:
-                self.__add_record(data)
-            elif data != '':
-                self.dataset.append(data)
+        data = data.strip()
+        if self.__interest and len(data) > 0:
+            data = re.sub(r'-*', '', data).strip()
+            if data in ["Montag", "Dienstag", "Mittwoch", "Donnerstag", "Freitag"]:
+                # Add a day as key to the dictionary:
+                self._current_list = self.schedule.setdefault(data, [{}])
+                self._current_idx = 0
+            elif data:
+                node = self._current_list[self._current_idx]
+                key = guess_key_from_data(data)
 
-    def get_table(self):
-        self.__add_record(self.lastday)
+                if node.get(key):
+                    node[key] += '#' + data
+                else:
+                    node[key] = data
 
-        struct = {}
-        for k,v in self.plandict.items():
-            record = []
-            part   = []
-            for data in v:
-                data = re.sub(r'-*','',data).strip()
-                part.append(data)
-                if re.search('F[A-Z][0-9]*',data):
-                    record.append(tuple(part))
-                    part = []
+                # Match a room like FD177, if so we open a new Node
+                if looks_like_room(data):
+                    self._current_idx += 1
+                    self._current_list.append({})
 
-            if len(v) != 0:
-                struct[k] = record
+    def feed(self, input_data):
+        HTMLParser.feed(self, input_data)
+        self.sanitize_dict()
 
-        return struct
+    def sanitize_dict(self):
+        for day, data in self.schedule.items():
+            for idx, node in enumerate(data):
+                if len(node) < 2:
+                    del self.schedule[day][idx]
+                    continue
 
-    def print_table(self,table):
-        for k,v in table.items():
-            print(k)
-            for t in v:
-                print('{time:18} {room:15} {name:32} {prof}'.format(
-                    time =  (t[0] + " bis " + t[1]),
-                    room = t[-1],
-                    name = t[2],
-                    prof = t[-3]))
+                # Sanitize time:
+                if node.get('time'):
+                    node['time'] = node['time'].replace('#', '-')
 
-        
-class Download():
-    def __init__(self,**param):
-        self.url = "http://www.hof-university.de/index.php?id=515&st={st}&fs={fs}&jahr=2012&semester=SS".format(**param)
-        try:
-            handle = urllib.request.urlopen(self.url)
-            self.data = str(handle.read(),'UTF-8')
-        except HTTPError as e:
-            print("Unable to load URL:",e,self.url)
-            sys.exit(-1)
-        except URLError as e:
-            print("Invalid URL:",e,self.url)
-            sys.exit(-2)
+                # Process _tmp
+                if node.get('_tmp'):
+                    split = node.get('_tmp').split('#')
+                    node['name'] = split[0]
+                    if len(split) > 1:
+                        node['prof'] = split[-1]
+                    if len(split) > 2:
+                        node['desc'] = split[1]
+                    del node['_tmp']
+
+    def print_table(self):
+        import json
+        print(json.dumps(self.schedule, indent=4))
+
+
+def download_schedule(studiengang, semester):
+    data = ''
+    url = "http://www.hof-university.de/index.php?id=515&st={st}&fs={fs}&jahr=2012&semester={ws}".format(
+            st=quote(studiengang),
+            fs=quote(semester),
+            ws='WS')
+    try:
+        print('# Used URL: ', url)
+        data = str(urlopen(url).read(), 'UTF-8')
+    except urllib.error.HTTPError as e:
+        print("Unable to load URL:", e, url)
+    except urllib.error.URLError as e:
+        print("Invalid URL:", e, url)
+    finally:
+        return data
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
         print('{} <Studiengang> <Fachsemester>'.format(sys.argv[0]))
+        sys.exit(-3)
 
-    urlinfo = {
-       'st': sys.argv[1],
-       'fs': sys.argv[2]
-    }
-
-    d = Download(**urlinfo)
-    
     parser = TableHTMLParser()
-    parser.feed(d.data)
-    parser.print_table(parser.get_table())
+    parser.feed(download_schedule(
+       studiengang=sys.argv[1],
+       semester=sys.argv[2]))
+
+    parser.print_table()
