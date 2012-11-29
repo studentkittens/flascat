0915d2ea1d9b0fbeeddc61308838691be4fd3cf2
kitteh <christoph@nullcat.de>
2012-11-29 19:01:27 +0100

    keine lust mehr, tintenfisch flash.

--- moosr/moosr.py
+++ moosr/moosr.py
@@ -4,12 +4,14 @@
 # Standard Modules
 import json
 import random
+import sqlite3
+from contextlib import closing
 
 # This is great.
 from functools import partial
 
 # Flask imports
-from flask import Flask, request, render_template, flash, redirect, url_for, Response
+from flask import Flask, request, render_template, flash, redirect, url_for, Response, g
 
 # Login
 from flask.ext.login import login_required
@@ -19,7 +21,6 @@ import helper
 import tagcloud
 import render
 import user_login
-import database
 
 
 # Instance the Flask Application itself
@@ -29,10 +30,16 @@ app = Flask(__name__)
 # echo '' | md5sum, actually, a rather bad key :-)
 app.secret_key = '68b329da9893e34099c7d8ad5cb9c940'
 
+# database configuration
+DATABASE = 'moosr.db'
+USERNAME = 'admin'
+PASSWORD = 'default'
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
 
 ###########################################################################
 #                            Routing functions                            #
 ###########################################################################
+
 
 @app.route('/do_search', methods=['POST', 'GET'])
 def do_search():
@@ -148,11 +155,6 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.route('/blog')
-def blog_page():
-    database.connect_db()
-
-
 @app.route('/')
 def main_page():
     '''
@@ -168,7 +170,45 @@ def main_page():
 
     return render_template('cloud.html', tags=tags)
 
+###########################################################################
 
+
+@app.route('/blog')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+
+@app.route('/add')
+def add_entry():
+
+    g.db.execute('insert into entries (title, text) values (?, ?)',
+                 ["das hier ist ein titel", "das hier ist ein wunderschoner text"])
+    g.db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('show_entries'))
+
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('database.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+def connect_db():
+    return sqlite3.connect('moosr.db')
+
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
 ###########################################################################
 #                               Let it run!                               #
 ###########################################################################
